976caebc30be4ef127d8b644fc9455f07b1f1eb8
giansoniaputra <giansonia555@gmail.com>
2023-03-21 07:31:44 +0700

    menyelesaikan Tampilan Mempelai

---
+++ js/animasi.js
@@ -0,0 +1,47 @@
+$(document).ready(function () {
+    $(".bunga .bg").addClass("muncul")
+    $(".bunga img").removeClass("top-100")
+    $(".bunga img").addClass("top-50")
+    $("#home h2").addClass("muncul")
+    $("#text").css({
+        'animation': 'muncul 3s ease',
+        'opacity': '100%'
+    })
+})
+$(window).scroll(function () {
+    let wScroll = $(this).scrollTop();
+    console.log(wScroll)
+    // Section Mempelai----------------
+    if(wScroll > 895){
+        // salam
+        $("#mempelai .salam").css({
+            'animation' : 'munculSalam 1s ease-in-out',
+        })
+        $("#mempelai .salam").addClass("muncul")
+        //tulisan
+        $("#mempelai .tulisan").addClass("muncul")
+        //foto mempelai
+        $("#mempelai .foto-pria").addClass("muncul")
+        $("#mempelai .foto-wanita").addClass("muncul")
+        // Nama Mempelai Pria
+        $("#mempelai .nama-mempelai-pria").css({
+            'animation' : 'munculSalam 1s ease-in-out',
+        })
+        $("#mempelai .nama-mempelai-pria").addClass("muncul")
+        // Nama Mempelai wanita
+        $("#mempelai .nama-mempelai-wanita").css({
+            'animation' : 'munculSalam 1s ease-in-out',
+        })
+        $("#mempelai .nama-mempelai-wanita").addClass("muncul")
+        //Ortu Memepelai
+        $("#mempelai .ortu-mempelai-pria").addClass("muncul")
+        $("#mempelai .ortu-mempelai-wanita").addClass("muncul")
+        //Love
+        $("#mempelai .love").css({
+            'animation' : 'munculSalam 1s ease-in-out',
+        })
+        $("#mempelai .love").addClass("muncul")
+
+
+    }
+})
